3b4c1ece9852c297ad99d0ce361a58663b1eb594
yuseong Nam <dbtjd6511@gmail.com>
2022-02-07 02:21:59 +0900

    🚀 Heroku : PORT variable

--- src/init.js
+++ src/init.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 import './db';
 import app from './server';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () =>
     console.log('✅ server is Ready! http://localhost:3000')
