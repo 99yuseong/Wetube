7dd94de5e901e9e6790354fd1dfb840b246d374c
yuseong Nam <dbtjd6511@gmail.com>
2022-02-06 23:10:51 +0900

    🐛 keydown space bar scroll error fixed

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -3,3 +3,10 @@ import regeneratorRuntime from 'regenerator-runtime';
 
 // header
 import './base/header';
+
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Space' && event.target === document.body) {
+        event.preventDefault();
+        return false;
+    }
+});
